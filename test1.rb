bfed668195dfb0b98a4cfd29cd27ae6fa432aafb
David <77647013+drampil@users.noreply.github.com>
2021-05-28 19:44:24 -0400

    Create test1.rb

---
+++ test1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+fruits = ['banana', 'apple', 'pear']
+
+fruits.each do |fruit|
+  if fruit == 'apple'
+    #exit!
+    #exit
+    abort("exit on apple")
+    #redo
+  end
+
+  puts fruit.capitalize
+end
+
+puts "Total fruits: #{fruits.count}"
